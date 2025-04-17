6096b9976244c464f6c965fb6fead64c85793d75
AgFelix <marcelo12543@hotmail.com>
2025-04-17 15:21:02 -0300

    Add rota backend deployment

--- src/components/AiSolution.tsx
+++ src/components/AiSolution.tsx
@@ -75,7 +75,7 @@ const AiSolution: React.FC = () => {
         setIsLoading(true);
 
         try {
-            const response = await fetch('http://localhost:8000/analisar', {
+            const response = await fetch('https://http-backend-wbdu.onrender.com/analisar', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
